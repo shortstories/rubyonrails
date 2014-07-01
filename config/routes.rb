1f7d7152f48f1171e072e758ad788a1e7ef84490
ohchang <kwonsci@gmail.com>
2014-07-02 00:53:12 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rcafe::Application.routes.draw do
+  resources :posts
+
+root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
